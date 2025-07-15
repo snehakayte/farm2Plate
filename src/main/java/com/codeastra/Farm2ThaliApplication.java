10238438ec64b31d4233b165f554c2627d3c4c84
Sneha Mathankar <snehakayte94@gmail.com>
2025-07-15 14:27:39 +0530

    Fixed log issue

--- src/main/java/com/codeastra/Farm2ThaliApplication.java
+++ src/main/java/com/codeastra/Farm2ThaliApplication.java
@@ -1,13 +1,12 @@
 package com.codeastra;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import lombok.extern.log4j.Log4j2;
 
 /**
  * @author Pratik Kayte
--- src/test/java/com/codeastra/Farm2ThaliApplicationTests.java
+++ src/test/java/com/codeastra/Farm2ThaliApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CodeastraApplicationTests {
+class Farm2ThaliApplicationTests {
 
 	@Test
 	void contextLoads() {
